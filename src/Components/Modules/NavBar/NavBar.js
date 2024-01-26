6ca89d3e754933029c488b5fe1dc5d7ab9267d00
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-01-26 16:50:00 +0330

    change some Styles

--- src/Components/Modules/NavBar/NavBar.js
+++ src/Components/Modules/NavBar/NavBar.js
@@ -3,9 +3,9 @@ import { FaUserCircle } from "react-icons/fa";
 export default function NavBar() {
     return (
         <>
-            <div className="  flex justify-between items-center p-5 z-30 bg-blue-600 border-b-1 border-zinc-200">
+            <div className="  flex justify-between items-center p-2 md:p-5 z-30 bg-blue-600 border-b-1 border-zinc-200">
                 <div className=' flex'>
-                    <img src="/images/png/logo.png" class=" w-40 " alt="FlowBite Logo" />
+                    <img src="/images/png/logo.png" class=" w-32 " alt="FlowBite Logo" />
                 </div>
                 <div className=" flex justify-center items-center gap-2 text-white">
                     <span>Username</span>
