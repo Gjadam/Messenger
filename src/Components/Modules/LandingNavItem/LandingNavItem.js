3690343ca8ebbf89fadb4caa1518bfcd6182f4b9
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-01-30 11:00:17 +0330

    Landing Navbar Items

---
+++ src/Components/Modules/LandingNavItem/LandingNavItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { RiArrowDropRightLine } from "react-icons/ri";
+export default function LandingNavItem({ text }) {
+    return (
+        <>
+            <a href="" className='group px-4 py-1 rounded-full text-gray-600 font-bold z-50 hover:bg-sky-200 hover:text-sky-600 transition-colors'>
+                <span className=' flex items-center relative  '><RiArrowDropRightLine className=' absolute -left-7 opacity-0 group-hover:opacity-100 group-hover:-left-4 transition-all mr-1 text-xl text-sky-600' />{text}</span>
+            </a>
+        </>
+    )
+}
