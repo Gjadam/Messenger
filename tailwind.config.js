cc2265fd42c1dc0adbb99a54401b249c8c746c6b
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-01-30 11:00:48 +0330

    Tailwind radial-gradients config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,9 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage : {
-        'bg-chat' : "url('/public/images/png/bg-chat.png')"
+        'bg-chat' : "url('/public/images/png/bg-chat.png')",
+        'radial-gradient-left': 'radial-gradient(rgba(186,230,253,0.6), rgba(186,230,253,0.4), transparent, transparent)',
+        'radial-gradient-right': 'radial-gradient(rgba(8,47,73,0.7), rgba(8,47,73,0.5), transparent, transparent)',
       },
       borderWidth : {
         '1' : '1px'
