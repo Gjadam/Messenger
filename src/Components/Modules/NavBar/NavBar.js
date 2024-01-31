494b9dbf6dffb52100038a4330c8aa4f709cbfc3
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-01-31 11:59:34 +0330

    add Options in NavBar

--- src/Components/Modules/NavBar/NavBar.js
+++ src/Components/Modules/NavBar/NavBar.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { FaUserCircle } from "react-icons/fa";
+import { FiMoreVertical } from "react-icons/fi";
+import { RiLogoutCircleRLine } from "react-icons/ri";
+import { IoMdSettings } from "react-icons/io";
 export default function NavBar() {
     return (
         <>
@@ -7,9 +10,22 @@ export default function NavBar() {
                 <div className=' flex'>
                     <img src="/images/png/logo.png" class=" w-32 " alt="FlowBite Logo" />
                 </div>
-                <div className=" flex justify-center items-center gap-2 text-white">
-                    <span>Username</span>
-                    <FaUserCircle className='text-4xl' />
+                <div className="group relative">
+                    <FiMoreVertical className='  text-white text-xl cursor-pointer ' />
+                    <div className=" opacity-0 hidden group-hover:block  group-hover:opacity-100 absolute right-2 top-5 w-36  rounded-md z-50  shadow bg-white overflow-hidden">
+                        <div className="flex justify-end items-center  gap-2 p-3 font-bold bg-gray-50 border-b-1">
+                            <span>Options</span>
+                            <IoMdSettings className='text-2xl text-gray-600' />
+                        </div>
+                        <div className="flex justify-end items-center  gap-2 p-3">
+                            <span className=' text-sm'>Username</span>
+                            <FaUserCircle className='text-xl text-blue-600' />
+                        </div>
+                        <div className="flex justify-end items-center  gap-2 p-3 group  cursor-pointer hover:bg-red-200 transition-colors">
+                            <span className=' group-hover:text-red-600 text-sm'>Logout</span>
+                            <RiLogoutCircleRLine className='text-xl text-red-600' />
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
