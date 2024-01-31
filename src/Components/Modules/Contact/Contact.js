58fdb2acbd2f1056ef81f33a9c1732f47a609cae
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-02-01 00:30:48 +0330

    DarkMode Logic

--- src/Components/Modules/Contact/Contact.js
+++ src/Components/Modules/Contact/Contact.js
@@ -4,8 +4,8 @@ import { FaUserCircle } from "react-icons/fa";
 export default function Contact() {
     return (
         <>
-            <div className=" flex justify-start items-center gap-2 text-black p-5 border-b-1 border-zinc-200 hover:bg-blue-100 transition-colors">
-                <FaUserCircle className='text-3xl text-blue-600' />
+            <div className=" flex justify-start items-center gap-2 text-black p-5 border-b-1 border-zinc-200 hover:bg-blue-100 transition-colors dark:text-white  dark:border-gray-950 dark:hover:bg-gray-950">
+                <FaUserCircle className='text-3xl text-blue-600 dark:text-gray-300' />
                 <span className=' font-bold'>username</span>
             </div>
         </>
