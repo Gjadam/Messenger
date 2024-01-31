3938b901f0b4ba8d74235d5a27484abfc93a18cc
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-02-01 00:30:33 +0330

    DarkMode Logic

--- src/Pages/Index/Index.js
+++ src/Pages/Index/Index.js
@@ -34,10 +34,10 @@ export default function Index() {
     return (
         <>
             <NavBar />
-            <div className=" flex h-[91.6vh] ">
+            <div className=" flex h-svh ">
             {/* Start SideBar */}
-                <form className=" w-96 md:block hidden bg-white border-r-1 border-zinc-200 ">
-                    <input type="search" placeholder='Search' className=' w-full border-b-1 outline-none px-5 py-6' />
+                <form className=" w-96 md:block hidden bg-white border-r-1 border-zinc-200  dark:bg-zinc-900 dark:border-zinc-950">
+                    <input type="search" placeholder='Search' className=' w-full border-b-1 outline-none px-5 py-6 dark:bg-zinc-900 dark:border-zinc-950 dark:text-white' />
                     <Contact />
                     <Contact />
                     <Contact />
@@ -46,12 +46,12 @@ export default function Index() {
                 </form>
             {/* End SideBar */}
             {/* Start SideBar For Mobile */}
-                <form className="side-bar w-0 opacity-0 invisible md:invisible fixed left-0 bottom-0 top-0 z-40 bg-white  transition-all">
-                    <div className=" flex justify-between items-center p-2  bg-blue-600">
+                <form className="side-bar w-0 opacity-0 invisible md:invisible fixed left-0 bottom-0 top-0 z-40 bg-white  transition-all dark:bg-zinc-900">
+                    <div className=" flex justify-between items-center p-2  bg-blue-600 dark:bg-zinc-950">
                         <img src="/images/png/logo.png" class=" w-32 " alt="FlowBite Logo" />
-                        <IoClose className='text-4xl text-white hover:text-red-700 transition-colors' onClick={closeSideBar}/>
+                        <IoClose className='text-4xl text-white hover:text-red-700 transition-colors cursor-pointer' onClick={closeSideBar}/>
                     </div>
-                    <input type="search" placeholder='Search' className=' w-full border-b-1 outline-none px-5 py-6' />
+                    <input type="search" placeholder='Search' className=' w-full border-b-1 outline-none px-5 py-6  dark:bg-zinc-900 dark:border-zinc-950 dark:text-white' />
                     <Contact />
                     <Contact />
                     <Contact />
@@ -61,9 +61,9 @@ export default function Index() {
             {/* End SideBar For Mobile */}
                 <div className=" flex justify-between  items-start flex-col  w-full  bg-contain bg-bg-chat relative">
                     {/* Start User Information */}
-                    <div className=" flex  justify-between items-center   w-full p-3 z-10 bg-white">
+                    <div className=" flex  justify-between items-center   w-full p-3 z-10 bg-white dark:bg-zinc-900 dark:text-white">
                         <div className=" flex items-center">
-                            <FaUserCircle className='text-5xl text-blue-600' />
+                            <FaUserCircle className='text-5xl text-blue-600 dark:text-zinc-300' />
                             <div className=" flex flex-col ms-2">
                                 <span className=' font-bold'>username</span>
                                 <div className=" flex justify-start items-center">
@@ -76,7 +76,7 @@ export default function Index() {
                             </div>
                         </div>
                         <div className="md:hidden" onClick={openSideBar}>
-                            <FaBarsStaggered className=' text-2xl text-blue-600' />
+                            <FaBarsStaggered className=' text-2xl text-blue-600 cursor-pointer' />
                         </div>
                     </div>
                     {/* End User Information */}
@@ -92,7 +92,7 @@ export default function Index() {
                             <span className=' absolute bottom-1 right-3 text-xs'>12:11</span>
                         </div>
                         {/* Contact Chats  */}
-                        <div className=" self-end max-w-96 rounded-s-xl rounded-br-3xl bg-gray-600 text-white px-3 pb-6 pt-3 my-2 relative">
+                        <div className=" self-end max-w-96 rounded-s-xl rounded-br-3xl bg-zinc-600 text-white px-3 pb-6 pt-3 my-2 relative">
                             <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem nihil doloremque dolorum quisquam inventore. Corrupti quia debitis reiciendis nostrum earum odit esse, aliquid doloribus modi officiis quo nemo et eaque.</span>
                             <span className=' absolute bottom-1 left-3 text-xs'>12:11</span>
                             <IoCheckmarkDoneOutline className=' absolute bottom-1 left-11 text-lg text-blue-400' />
@@ -101,8 +101,8 @@ export default function Index() {
                     {/* End Chat */}
                     {/* Start Input Chat */}
                     <div className=" flex justify-between items-center w-full ">
-                        <input type="text" placeholder='Message' className='  w-full h-9 px-3  outline-none border-t-1 border-b-0 border-zinc-200 bg-white ' />
-                        <button className='absolute right-3 bottom-0 '>
+                        <input type="text" placeholder='Message' className='  w-full h-12 px-3  outline-none border-t-1 border-b-0 border-zinc-200 bg-white dark:bg-zinc-900 dark:border-zinc-950 dark:text-white' />
+                        <button className='absolute right-3 bottom-2 '>
                             <RiSendPlaneFill className='  text-blue-600 text-2xl' />
                         </button>
                     </div>
