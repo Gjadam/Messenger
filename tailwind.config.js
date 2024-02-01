d24a90b7ca98641b64be819e77116acee1d5804e
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-02-02 01:30:54 +0330

    add bg for NotFound Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage : {
+        'bg-Notfound' : "url('/public/images/png/NotFound-bg.png')",
         'bg-chat' : "url('/public/images/png/bg-chat.png')",
         'radial-gradient-left': 'radial-gradient(rgba(186,230,253,0.6), rgba(186,230,253,0.4), transparent, transparent)',
         'radial-gradient-right': 'radial-gradient(rgba(8,47,73,0.7), rgba(8,47,73,0.5), transparent, transparent)',
