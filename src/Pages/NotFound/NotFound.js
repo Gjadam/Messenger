474c1593fb6132a87b64049c30bc3f796bbf813a
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-02-02 01:34:23 +0330

    Create NotFound Page

---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import NavBar from '../../Components/Templates/NavBar/NavBar'
+import { Link } from 'react-router-dom'
+import { IoIosHome } from "react-icons/io";
+export default function NotFoundNotFound() {
+  return (
+    <div className=' flex justify-center items-center w-full h-svh bg-bg-Notfound bg-no-repeat bg-center overflow-hidden'>
+      <NavBar />
+      <div className=" container flex justify-center items-center flex-col gap-12">
+        <img src="/images/png/NotFound.png" alt="NotFound" className=' w-48 md:w-96' />
+        <span className=' tracking-wide text-center text-5xl font-bold text-lg  md:text-3xl text-white'>Ooops...Something went wront</span>
+        <Link to={'/'} className=' rounded-full p-5 text-sky-600 bg-white font-bold  hover:bg-sky-600 hover:text-white transition-colors'>
+          <span className=' flex items-center gap-1'><IoIosHome className=' text-2xl' /> Go To Home</span>
+        </Link>
+      </div>
+    </div>
+  )
+}
