32699974da20ad3ddfc0ec485b42e2c38206639a
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-02-02 01:32:12 +0330

    Add new routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import Index from "./Pages/Index/Index";
 import Landing from "./Pages/Landing/Landing";
-
+import Login from "./Pages/Login/Login";
+import NotFound from './Pages/NotFound/NotFound'
+import SignUp from "./Pages/SignUp/SignUp";
 const routes= [
     { path: '/', element: <Landing /> },
     { path: '/chat', element: <Index /> },
+    { path: '/Login', element: <Login /> },
+    { path: '/Sign-up', element: <SignUp /> },
+    { path: '*', element: <NotFound /> },
 ]
 
 export default routes
