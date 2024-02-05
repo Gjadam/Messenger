29726373041c69abf98757ef27a101b37d1fc2aa
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-02-06 01:44:29 +0330

    login and signup Validation

--- src/Components/Templates/NavBar/NavBar.js
+++ src/Components/Templates/NavBar/NavBar.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import LandingNavItem from '../../Modules/LandingNavItem/LandingNavItem'
+import AuthContext from '../../../context/authContext'
 export default function NavBar() {
+
+  const authContext = useContext(AuthContext)
+
   return (
     <div className=" fixed top-0 right-0 left-0  z-50  container m-auto p-8 flex justify-between items-center ">
       <div className=" flex items-center ">
-        <LandingNavItem text={"Login"} href={"/Login"} />
-        <LandingNavItem text={"Sign up"} href={"/Sign-up"} />
+        {
+          authContext.isLoggedIn ? (
+            <LandingNavItem text={"Go to Chat"} href={"/chat"} />
+          ) : (
+            <>
+            <LandingNavItem text={"Login"} href={"/login"} />
+            <LandingNavItem text={"Sign up"} href={"/sign-up"} />    
+            </>
+          )
+        }
+        
       </div>
       <a href='' className=" hidden md:block">
         <img src="/images/png/logo.png" alt="logo" className=' w-32' />
