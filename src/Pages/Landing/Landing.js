4bf7c85db564d0d080a49519110808ff4dbe2390
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-02-08 01:33:09 +0330

    change some style

--- src/Pages/Landing/Landing.js
+++ src/Pages/Landing/Landing.js
@@ -27,7 +27,7 @@ export default function Landing() {
                     <div className=" flex justify-center md:justify-start items-center flex-wrap gap-2 mt-7  ">
                         {
                             authContext.isLoggedIn ? (
-                                <Link to="/chat" className=' bg-sky-500 rounded-full px-4 py-1 text-white hover:text-sky-500 hover:bg-transparent transition-colors'>
+                                <Link to="/chat/id" className=' bg-sky-500 rounded-full px-4 py-1 text-white hover:text-sky-500 hover:bg-transparent transition-colors'>
                                     <span className=' flex items-center'><TbExternalLink className=' mr-1' /> Open ChatOnly Web</span>
                                 </Link>
 
@@ -53,7 +53,7 @@ export default function Landing() {
             </div>
             {/* End Main */}
             {/* Start Footer */}
-            <div className=" fixed bottom-5 left-5 font-bold text-center z-50">
+            <div className=" fixed bottom-2 left-2 font-bold text-center z-50">
                 <span className=' text-sm ' >
                     &copy; Created By
                     <a href="https://github.com/Gjadam" className=' mx-1 hover:text-sky-500 transition-colors'>
