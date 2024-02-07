b14d80d9b50f503b3750b2c79fd75056e50fabc1
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-02-07 21:25:15 +0330

    change styles and add lastSeen logic

--- src/Components/Modules/Contact/Contact.js
+++ src/Components/Modules/Contact/Contact.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { FaUserCircle } from "react-icons/fa";
 
-export default function Contact() {
+export default function Contact({ username, lastOnline }) {
     return (
         <>
-            <div className=" flex justify-start items-center gap-2 text-black p-5 border-b-1 border-zinc-200 hover:bg-blue-100 transition-colors dark:text-white  dark:border-gray-950 dark:hover:bg-gray-950">
-                <FaUserCircle className='text-3xl text-blue-600 dark:text-gray-300' />
-                <span className=' font-bold'>username</span>
+            <div className=" flex justify-start items-center gap-2 text-black py-3 px-5 hover:bg-blue-100 transition-colors dark:text-white dark:hover:bg-gray-950">
+                <FaUserCircle className='text-4xl text-blue-600 dark:text-gray-300' />
+                <div className=" flex flex-col ">
+                    <span className=' font-bold'>{username}</span>
+                    <span className=' text-xs text-zinc-500'>{lastOnline}</span>
+                </div>
             </div>
         </>
     )
