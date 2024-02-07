8837ff9903c48c17faa31c81d0cf12b52dc02d68
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-02-08 01:32:17 +0330

    get contacts from server

--- src/Components/Modules/SideBar/SideBar.js
+++ src/Components/Modules/SideBar/SideBar.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import Contact from '../Contact/Contact'
 import { MdErrorOutline } from "react-icons/md";
+import { BsEmojiSmile } from "react-icons/bs";
+import { NavLink } from 'react-router-dom';
 export default function SideBar() {
     const [searchValue, setSearchValue] = useState('')
     const [searchData, setSearchData] = useState([])
+    const [contacts, setContacts] = useState([])
     const localStorageData = JSON.parse(localStorage.getItem("user"))
 
     const getSearchData = () => {
@@ -26,7 +29,22 @@ export default function SideBar() {
         }
     }, [searchValue])
 
-    console.log(searchData);
+    useEffect(() => {
+        fetch(`https://chattak-alirh.koyeb.app/users/contacts/`, {
+            headers: {
+                'Authorization': `Bearer ${localStorageData.token}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                setContacts(data)
+            })
+    }, [searchValue])
+
+
+
+
+
     return (
         <>
             <div className="">
@@ -34,30 +52,39 @@ export default function SideBar() {
                 {
                     searchValue.length ? (
                         searchData.length > 0 ? (
-                        <div className=" bg-zinc-100">
-                            {
-                                searchData?.map(search => (
-                                    <Contact username={search.username} lastOnline={search.last_online} />
-                                ))
-                            }
+                            <div className=" bg-zinc-100 dark:bg-zinc-800">
+                                {
+                                    searchData?.map(search => (
+                                        <Contact key={search.id} userID={search.id} username={search.username} lastOnline={search.last_online} type={'search'} />
+                                    ))
+                                }
 
-                        </div>
+                            </div>
 
                         ) : (
-                            <div className=" flex justify-center items-center flex-col gap-2 mt-10">
-                                <MdErrorOutline className=' text-zinc-400 text-6xl' />
-                                <span className=' text-zinc-400'>This user could not be found</span>
+                            <div className=" flex justify-center items-center flex-col gap-2 mt-10 p-5">
+                                <MdErrorOutline className=' text-zinc-600 text-6xl' />
+                                <span className=' text-zinc-600'>This user could not be found</span>
                             </div>
                         )
                     ) : (
                         <>
-                            <Contact username={'test name'} lastOnline={"long time ago"} />
-                            <Contact username={'test name'} lastOnline={"long time ago"} />
-                            <Contact username={'test name'} lastOnline={"long time ago"} />
-                            <Contact username={'test name'} lastOnline={"long time ago"} />
-                            <Contact username={'test name'} lastOnline={"long time ago"} />
-                            <Contact username={'test name'} lastOnline={"long time ago"} />
-                            <Contact username={'test name'} lastOnline={"long time ago"} />
+                            {
+                                contacts.length ? (
+                                    contacts.map(contact => (
+                                        <Contact key={contact.id} userID={contact.id} username={contact.username} lastOnline={contact.last_online} type={'contact'} />
+                                    ))
+                                ) : (
+                                    <div className=" flex justify-center items-center flex-col gap-2 mt-10 p-5">
+                                        <BsEmojiSmile className=' text-zinc-700 text-6xl dark:text-zinc-300' />
+                                        <span className=' text-zinc-700 font-bold text-center dark:text-zinc-300'>You have no contacts on OnlyChat yet</span>
+                                        <ul className=' list-disc mt-3'>
+                                            <li className='text-zinc-500 dark:text-zinc-400 text-sm leading-6'>Search people by username </li>
+                                            <li className='text-zinc-500 dark:text-zinc-400 text-sm leading-6'>Invite friends to try OnlyChat</li>
+                                        </ul>
+                                    </div>
+                                )
+                            }
                         </>
 
                     )
