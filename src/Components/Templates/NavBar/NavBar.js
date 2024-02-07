060edf11f2a60a913a55ac56d761133852d6db11
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-02-07 21:26:28 +0330

    change  styles

--- src/Components/Templates/NavBar/NavBar.js
+++ src/Components/Templates/NavBar/NavBar.js
@@ -1,24 +1,33 @@
 import React, { useContext } from 'react'
 import LandingNavItem from '../../Modules/LandingNavItem/LandingNavItem'
 import AuthContext from '../../../context/authContext'
+import { MdLogin } from 'react-icons/md'
+import { BiSolidPencil } from "react-icons/bi";
+import { TbExternalLink } from 'react-icons/tb';
+import { RiLogoutCircleRLine } from "react-icons/ri";
 export default function NavBar() {
 
   const authContext = useContext(AuthContext)
+
+
 
   return (
     <div className=" fixed top-0 right-0 left-0  z-50  container m-auto p-8 flex justify-between items-center ">
       <div className=" flex items-center ">
         {
           authContext.isLoggedIn ? (
-            <LandingNavItem text={"Go to Chat"} href={"/chat"} />
+            <>
+              <LandingNavItem text={"Open Chat"} icon={<TbExternalLink className=' mr-1 ' />} href={"/chat"} />
+              <LandingNavItem text={"Logout"} icon={<RiLogoutCircleRLine className=' mr-1 ' />} href={"/"} />
+
+            </>
           ) : (
             <>
-            <LandingNavItem text={"Login"} href={"/login"} />
-            <LandingNavItem text={"Sign up"} href={"/sign-up"} />    
+              <LandingNavItem text={"Login"} icon={<MdLogin className=' mr-1 ' />} href={"/login"} />
+              <LandingNavItem text={"Sign up"} icon={<BiSolidPencil className=' mr-1 ' />} href={"/sign-up"} />
             </>
           )
         }
-        
       </div>
       <a href='' className=" hidden md:block">
         <img src="/images/png/logo.png" alt="logo" className=' w-32' />
