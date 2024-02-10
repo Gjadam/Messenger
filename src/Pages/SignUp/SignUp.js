ca0e8bef7e5ad5c507a16dc1e0feaa5654b5ceec
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-02-10 13:32:14 +0330

    comment validation

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -62,21 +62,21 @@ export default function SignUp() {
           }
         })
     },
-    validate: (values) => {
-      const errors = {}
-      if (values.username === '') {
-        errors.username = 'Username is required!'
-      } else if (values.username.length < 4) {
-        errors.username = 'Username must be at least 4 characters long!'
-      }
+    // validate: (values) => {
+    //   const errors = {}
+    //   if (values.username === '') {
+    //     errors.username = 'Username is required!'
+    //   } else if (values.username.length < 4) {
+    //     errors.username = 'Username must be at least 4 characters long!'
+    //   }
 
-      if (values.password === '') {
-        errors.password = 'Password is required!'
-      } else if (values.password.length < 8) {
-        errors.password = 'Password must be at least 8 characters long!'
-      }
-      return errors
-    }
+    //   if (values.password === '') {
+    //     errors.password = 'Password is required!'
+    //   } else if (values.password.length < 8) {
+    //     errors.password = 'Password must be at least 8 characters long!'
+    //   }
+    //   return errors
+    // }
   })
 
 
