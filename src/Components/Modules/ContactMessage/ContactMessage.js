2c8139a5a654c7d4b8425d2e9b5bab194e6536e4
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-02-13 00:13:04 +0330

    adding time

--- src/Components/Modules/ContactMessage/ContactMessage.js
+++ src/Components/Modules/ContactMessage/ContactMessage.js
@@ -7,7 +7,13 @@ export default function ContactMessage(props) {
 
             <div className="  break-words  max-w-96 self-end rounded-e-xl rounded-bl-xl bg-zinc-600 text-white px-3 pb-6 pt-3 my-2 relative">
                 <p>{props.text}</p>
-                <span className=' absolute bottom-1 left-3 text-xs'>{props.date_send?.slice(11, 16)}</span>
+                {
+                        props.role === "Contact" ? (
+                            <span className=' absolute bottom-1 right-3 text-xs'>{props.date_send}</span>
+                        ) : (
+                            <span className=' absolute bottom-1 right-3 text-xs'>{props.date_send?.slice(11, 16)}</span>
+                        )
+                    }
             </div>
         </div>
         </>
