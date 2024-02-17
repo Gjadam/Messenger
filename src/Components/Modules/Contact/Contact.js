dd3c91cf32cffcd49629b0a90883d981ae151260
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-02-18 00:00:26 +0330

    Last Online logic

--- src/Components/Modules/Contact/Contact.js
+++ src/Components/Modules/Contact/Contact.js
@@ -1,10 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { PiUserCircleFill } from "react-icons/pi";
 import { AiOutlineUserAdd } from "react-icons/ai";
+import { MdDataSaverOn } from "react-icons/md";
 import Swal from 'sweetalert2';
 import { NavLink } from 'react-router-dom';
+import AuthContext from '../../../context/authContext';
 export default function Contact({ chatID, userID, username, lastOnline, type }) {
+
     const localStorageData = JSON.parse(localStorage.getItem("user"))
+    const authContext = useContext(AuthContext)
+    // Last Online logic
+    const currentDate = new Date();
+    const lastOnlineDate = new Date(lastOnline);
+
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth();
+    const currentDay = currentDate.getDate();
+
+    const lastOnlineYear = lastOnlineDate.getFullYear();
+    const lastOnlineMonth = lastOnlineDate.getMonth();
+    const lastOnlineDay = lastOnlineDate.getDate();
+
+    const currentTimestamp = new Date(currentYear, currentMonth, currentDay).getTime();
+    const lastOnlineTimestamp = new Date(lastOnlineYear, lastOnlineMonth, lastOnlineDay).getTime();
+
+    const timeDiff = currentTimestamp - lastOnlineTimestamp;
+    const dayDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
 
     const addToContact = () => {
         fetch(`https://chattak-alirh.koyeb.app/users/contacts/${userID}/`, {
@@ -38,17 +59,38 @@ export default function Contact({ chatID, userID, username, lastOnline, type }) 
 
     return (
         <>
-            <NavLink to={`/chat/${userID}/${typeof chatID === "number" ? chatID : ''}`} className={(Link) => Link.isActive && ' block bg-blue-400 text-white dark:bg-zinc-700'}>
+            <NavLink to={`/chat/${userID}${typeof chatID === "number" ? `/${chatID}` : ''}`} className={(Link) => Link.isActive && ' block bg-blue-400 text-white dark:bg-zinc-700'}>
                 <div className="  hover:bg-blue-300 dark:hover:bg-zinc-500 hover:text-white transition-colors dark:text-white ">
                     <div className=" flex justify-between items-center w-full py-3 px-5 border-b-1 dark:border-zinc-950">
 
                         <div className=" flex justify-start items-center gap-2">
-                            <PiUserCircleFill className='text-4xl text-blue-700 dark:text-gray-300' />
+                            {
+                                username === authContext.userInfos.username ? (
+                                    <MdDataSaverOn className='text-4xl text-blue-700 dark:text-gray-300' />
+                                ) : (
+                                    <PiUserCircleFill className='text-4xl text-blue-700 dark:text-gray-300' />
+                                )
+                            }
                             <div className=" flex flex-col ">
-                                <span className=' font-bold'>{username.slice(0, 15)}{username.length > 15 && "..."}</span>
+                                {
+                                    username === authContext.userInfos.username ? (
+                                        <span className=' font-bold'>Saved Messages</span>
+                                    ) : (
+                                        <span className=' font-bold'>{username.slice(0, 15)}{username.length > 15 && "..."}</span>
+                                    )
+                                }
                                 {
                                     type === 'contact' &&
-                                    <span className=' text-xs text-zinc-500 dark:text-zinc-200'>last online : {lastOnline.slice(0, 10)}</span>
+                                        username === authContext.userInfos.username ? (
+                                        null
+                                    ) : (
+
+                                        dayDiff > 7 ? (
+                                            <span className=' text-xs text-zinc-500 dark:text-zinc-200'>last seen a long time ago</span>
+                                        ) : (
+                                            <span className=' text-xs text-zinc-500 dark:text-zinc-200'>last seen recently</span>
+                                        )
+                                    )
                                 }
                             </div>
                         </div>
