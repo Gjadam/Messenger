f8923b2c8a3e96f35fb078402135d3af00764c65
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-02-18 00:00:59 +0330

    changing message box styles

--- src/Components/Modules/ContactMessage/ContactMessage.js
+++ src/Components/Modules/ContactMessage/ContactMessage.js
@@ -3,19 +3,22 @@ import React from 'react'
 export default function ContactMessage(props) {
     return (
         <>
-        <div className="flex justify-start items-center">
+            <div className="flex justify-start items-center">
 
-            <div className="  break-words  max-w-96 self-end rounded-e-xl overflow-hidden rounded-bl-xl bg-zinc-600 text-white px-3 pb-6 pt-3 my-2 relative">
-                <p>{props.text}</p>
-                {
-                        props.role === "Contact" ? (
-                            <span className=' absolute bottom-1 right-3 text-xs'>{props.date_send}</span>
-                        ) : (
-                            <span className=' absolute bottom-1 right-3 text-xs'>{props.date_send?.slice(11, 16)}</span>
-                        )
-                    }
+                <div className="  break-words  max-w-96 self-end rounded-e-xl overflow-hidden rounded-bl-xl bg-zinc-600 text-white px-3 py-4 my-2 ">
+                    <p>{props.text}</p>
+                    <div className="flex items-center mt-1 gap-1">
+                        {
+                            props.role === "Contact" ? (
+                                <span className='text-xs'>{props.date_send}</span>
+                            ) : (
+                                <span className='text-xs'>{props.date_send?.slice(11, 16)}</span>
+                            )
+                        }
+                    </div>
+
+                </div>
             </div>
-        </div>
         </>
     )
 }
