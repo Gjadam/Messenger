f065ffc6354c22108bbcb628e76feb4d768b8bec
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-03-01 00:29:03 +0330

    getting lastMessage

--- src/Components/Modules/Contact/Contact.js
+++ src/Components/Modules/Contact/Contact.js
@@ -6,7 +6,7 @@ import Swal from 'sweetalert2';
 import { NavLink } from 'react-router-dom';
 import AuthContext from '../../../context/authContext';
 
-export default function Contact({ chatID, userID, username, lastOnline, type }) {
+export default function Contact({ chatID, userID, username, lastMessage, type }) {
 
     const localStorageData = JSON.parse(localStorage.getItem("user"))
     const authContext = useContext(AuthContext)
@@ -51,11 +51,11 @@ export default function Contact({ chatID, userID, username, lastOnline, type }) 
             <NavLink to={`/chat/${userID}${typeof chatID === "number" ? `/${chatID}` : ''}`} className={(Link) => Link.isActive && ' block bg-blue-400 text-white dark:bg-zinc-700'} >
                 <div className="  hover:bg-blue-300 dark:hover:bg-zinc-500 hover:text-white transition-colors dark:text-white ">
                     <div className=" flex justify-between items-center w-full py-3 px-5 border-b-1 dark:border-zinc-950">
-
                         <div className=" flex justify-start items-center gap-2">
                             <PiUserCircleFill className='text-4xl text-blue-700 dark:text-gray-300' />
-                            <div className=" flex flex-col ">
-                                <span className=' select-none font-bold'>{username.slice(0, 15)}{username.length > 15 && "..."}</span>
+                            <div className=" flex flex-col select-none ">
+                                <span className='  font-bold'>{username.slice(0, 15)}{username.length > 15 && "..."}</span>
+                                <span className=' text-sm text-zinc-400'>{lastMessage?.slice(0, 35)}{lastMessage?.length > 35 && '...'}</span>
                             </div>
                         </div>
                         {
@@ -66,7 +66,7 @@ export default function Contact({ chatID, userID, username, lastOnline, type }) 
                         }
                     </div>
                 </div>
-                
+
             </NavLink>
         </>
     )
